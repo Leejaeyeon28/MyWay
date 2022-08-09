f7dfe739a6d4ea7bfc4f630337bfbd683751d25a
SeonJeong <lkjhmnb72714@gmail.com>
2022-08-09 20:48:09 +0900

    스플래시, 상단바 없애기

---
+++ app/src/main/java/com/example/myway/EmptySeat.java
@@ -0,0 +1,14 @@
+package com.example.myway;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EmptySeat extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_empty_seat);
+    }
+}
